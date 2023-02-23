290dc6e4ec97247c71d0cbd5d34843fac4a9e658
Yari <89977873+yari-iray@users.noreply.github.com>
2023-02-23 10:02:37 +0100

    Initial start: - changed for loops to list comprehensions (for now) -> generates more optimized byte-code which can be much faster, might have to be changed later
    - Added a node class which serves as the basis for an objects

--- Machine Learning Project/MainProgram.py
+++ Machine Learning Project/MainProgram.py
@@ -6,9 +6,6 @@ import matplotlib as mlt
 import pandas as pd
 import sklearn
 
-class MainClass:
-    self.Dit
-    self.Dat
 
 def Distance(node1, node2) -> float:
     n: int = len(node1)
@@ -23,28 +20,16 @@ def Distance(node1, node2) -> float:
 def FindNearestNeighbours(node, neighbours: list, k: int):
     # find and return k nearest neighbours to node
     # order the list and take k neares ones
-    distances = []
 
-    for neighbour in neighbours:
-        dist = Distance(node, neighbour)
-        distances.append(dist)
+    #obj with 
+    distances = [Distance(node, neighbour) for neighbour in neighbours]
+    
 
+    # Will rewrite to a node object with neighbours as list, 
+    # parameters is a question, maybe a list of features but could be long to implement and non-uniform
     return distances.index(min(distances))
 
 
-
-def main():
-    k = 7
-    x = [1,2,3,4,5,6]
-    y = [2,3,1,7,8,6]
-
-
-
-def DezeFunctieDoetDit():
-    camelCase: int = 3
-
-def add(x: int,y: int):
-    return x + y
-
-    print("Hello hello")
-
+def DistanceFaster(node1, node2) -> float:
+    x = sum( [ (node1[i] - node2[i] ** 2) for i in range((len(node1))) ])
+    return sqrt(x)
---
+++ Machine Learning Project/Objects.py
@@ -0,0 +1,10 @@
+from dataclasses import dataclass
+
+dataclass
+class Node:
+    Neighbours: list(Node)
+
+    Parameters: list
+
+
+

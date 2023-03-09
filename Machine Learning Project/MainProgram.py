5509e35da625da53df8e7fcd594d52d0181f162e
Yari <89977873+yari-iray@users.noreply.github.com>
2023-03-09 10:06:46 +0100

    - Refactored code & added type hints - Use global K, doesn't have to be passed as parameter - Added and removed some comments - Added milk dataset to the solution and python project, path does not have to be specified explicitly anymore (yay)

--- Machine Learning Project/MainProgram.py
+++ Machine Learning Project/MainProgram.py
@@ -1,58 +1,54 @@
-from math import sqrt
-#from operator import indexOf
-#from re import M
-#import numpy as np
-#import matplotlib as mlt
+import math
 import pandas as pd
 
 DataSplit: dict = {"training": 0.6, "validation": 0.2, "test": 0.2 }
-K: int = 5 #temporary value, just so we have one right now
-PathToDataset: str = "C:\\Users\\Yari\\OneDrive - Vrije Universiteit Amsterdam\\source\\repos\\Machine Learning Project\\Machine Learning Project\\milknew.csv"
+K: int = 5
+Dataset: str = "milknew.csv"
 
-def FindNearestNeighbours(DataFrame, Node, k: int):
-    # find and return k nearest neighbours to node
-    # order the list and take k neares ones
+def FindNearestNeighbours(DataFrame, Node):
+    # find and return k nearest neighbours to node by ordering list and taking the k nearest ones
     NeighbourList = DataFrame #Create a separate Dataframe to store the distance to the given Node
-    NeighbourList['Distance'] = 99999 #Set the distance to a very high number at first.
+    NeighbourList['Distance'] = math.inf #Initialize as infinity
 
     #Find all the distances to the points in the dataframe from Node.
     for i in range(0, DataFrame.shape[0]):
         NeighbourList.iloc[i,NeighbourList.shape[1]-1] = Distance(Node, DataFrame.iloc[i,:])
     
     NeighbourList = NeighbourList.sort_values(by='Distance', ascending=True) #Sort the dataframe by distance
-    NeighbourList = NeighbourList.iloc[0:k,:] #Keep only the k lowest distances, and return them.
+    NeighbourList = NeighbourList.iloc[0:K,:] #Keep only the K lowest distances, and return them.
     
     return NeighbourList
 
-def ClassifyNewNode(NearNeighbours, node):
-    GradeList: list = NearNeighbours.iloc[:,NearNeighbours.shape[1]-2].tolist() #Get a list of all grades (duplicates included)    
-    GradeCount: dict = {i: GradeList.count(i) for i in GradeList} #Count all occurences in the GradeList, storing them in a dictionary
-    
+def ClassifyNewNode(NearestNeighbours: pd.DataFrame, node):
+    Grades: list = NearestNeighbours.iloc[:,NearestNeighbours.shape[1]-2].tolist() #Get a list of all grades (duplicates included)    
+    GradeCount: dict = {i: Grades.count(i) for i in Grades} #Count all occurences in the GradeList, storing them in a dictionary
+
     newnode = node #new node as not to alter the actual dataset
-    newnode['Distance'] = sorted(GradeCount)[0] #set the value of the node to the most common Grade found in the Neighbours
+    newnode['Distance'] = sorted(GradeCount)[0] #set value to most occurring grade found in the neighbours
 
     return newnode
 
-def KNN(TrainData, TestData, k: int):
+def KNN(TrainData, TestData):
     Results = TestData
     for i in range(0, TestData.shape[0]):
-        NN = FindNearestNeighbours(TrainData, TestData.iloc[i,:], k)
+        NN = FindNearestNeighbours(TrainData, TestData.iloc[i,:])
         Results.iloc[i,:] = ClassifyNewNode(NN, TestData.iloc[i,:-1])
 
     return Results
 
 
 def Distance(node1, node2) -> float:
-    x = sum( [(node1[i] - node2[i]) ** 2 for i in range((len(node1) - 1))] )
-    return sqrt(x)
+    #for all parameters in the node, square the distance between node1 and node2, sum the distance values together
+    x = sum([(node1[i] - node2[i]) ** 2 for i in range((len(node1) - 1))])
+    return math.sqrt(x)
 
 
 class DataFunctions:
     def LoadDataset():
-        CsvData = pd.read_csv(PathToDataset)
-        Length: int = len(CsvData) #Needed to make a good split of data
+        CsvData = pd.read_csv(Dataset)
+        Length: int = len(CsvData) #needed to split the data into parts
 
-        #The data has to be split into training, validation and test data using predefined percentages.
+        #Split data into train, validation, test
         TrainData = CsvData[0: int(Length * DataSplit["training"])] 
         ValidationData = CsvData[int(Length * DataSplit["training"]): int(Length * DataSplit["training"]) + int(Length * DataSplit["validation"])]
         TestData = CsvData[int(Length * DataSplit["training"]) + int(Length * DataSplit["validation"]): Length] 
@@ -61,11 +57,13 @@ class DataFunctions:
 
 
     def NormalizeData(DataSet):
-        n: int = len(DataSet.columns) #Find the columns so we know how far to iterate
+        n: int = len(DataSet.columns) #Number of columns
 
-        for i in range(n-1): #range is n-1 because the last column does not contain numbers, but the class.
-            LowerBound: int = min(DataSet.iloc[:,i]) #Find the minimum value in the current column
-            UpperBound: int = max(DataSet.iloc[:,i]) #Find the maximum value in the current column
+        #the last column contains the grade, which doesn't need to be normalized as it is a classification and not a number
+        #loop over all columns and normalize them
+        for i in range(n - 1): 
+            LowerBound: int = min(DataSet.iloc[:,i]) #Column min
+            UpperBound: int = max(DataSet.iloc[:,i]) #Column max
 
             DataSet.iloc[:,i] = (DataSet.iloc[:,i] - LowerBound) / (UpperBound - LowerBound) #Using this function, the column will be normalized to be between 0 and 1
     
@@ -77,11 +75,11 @@ def Main():
     ValidationData = DataFunctions.NormalizeData(ValidationData)
     TestData = DataFunctions.NormalizeData(TestData)
 
-    print("Test data\n")
+    print("Test data")
     print(TestData)
 
-    print("KNN\n")
-    print(KNN(TrainData, TestData, K))
+    print("KNN classification")
+    print(KNN(TrainData, TestData))
 
 if __name__ == "__main__":
     Main()

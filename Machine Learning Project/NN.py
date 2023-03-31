b63855450a9c5b3092606a4b520237a4f53bd59d
axeleth <axeleth@me.com>
2023-03-31 22:48:10 +0200

    This is the NN I mentioned in the report.

--- Machine Learning Project/NN.py
+++ Machine Learning Project/NN.py
@@ -63,17 +63,6 @@ def tfNN(TrainData):
                 epochs=10)
         
         model.evaluate(TestData.iloc[:,:-1].values, TestData.iloc[:,-1].values, verbose=2)
-
-def skNN(TrainData):
-      X = [[0.,0.], [1.,1.]]
-      y = [0, 1]
-      clf = MLPClassifier(solver='lbfgs',
-                           alpha=1e-5, 
-                           hidden_layer_sizes=(5, 2), 
-                           random_state=1)
-      clf.fit(X, y)
-
-      clf.predict([[2., 2.], [-1., -2.]])
-
+        
 tfNN(TrainData)
 
